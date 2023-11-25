f998b140f1d0e5136928c463b3db6d0017abba16
Lou Onezime <lou.onezime@epitech.eu>
2023-11-25 15:28:47 +0100

    feat: :sparkles: working start of game that highlights words
    basic functions

---
+++ front-end/src/pages/Home.tsx
@@ -0,0 +1,52 @@
+import { Center, Text, VStack, HStack, Button } from '@chakra-ui/react';
+import { ReactElement, useState } from 'react';
+
+import { Canvas } from '@react-three/fiber';
+
+import Box from './HomePage/Cubes';
+import { useNavigate } from 'react-router-dom';
+
+const Title = () : ReactElement => {
+    const [hovered, hover] = useState(false);
+    const navigate = useNavigate();
+
+    const navigateToGame = () => {
+        navigate('/start');
+    };
+
+    return (
+        <VStack spacing="1px">
+            <Text
+                onMouseEnter={() => hover(true)}
+                onMouseLeave={() => hover(false)}
+                color={hovered ? 'darkred' : 'black'}
+                fontSize='6xl' as='b'>
+                    type tier</Text>
+            <Button onClick={(navigateToGame)} variant='link' fontSize='xl' color='darkred'>
+                test your typing speed
+            </Button>
+        </VStack>
+    );
+}
+
+const HomePage = () : ReactElement => {
+    return (
+        <Center mt="220px">
+            <HStack spacing='1px'>
+                <Title />
+                <Text>test</Text>
+                <VStack spacing='1px'>
+                    <Canvas>
+                        <ambientLight />
+                        <pointLight position={[10, 10, 10]} />
+                        <Box position={[-2.0, 0, 0]} />
+                        <Box position={[0.0, 0, 0]} />
+                        <Box position={[2.0, 0, 0]} />
+                    </Canvas>
+                </VStack>
+            </HStack>
+        </Center>
+    );
+}
+
+export default HomePage;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,22 @@
-import { Center, Text, VStack, HStack, Button } from '@chakra-ui/react';
+import { Center, Text, VStack, Button } from '@chakra-ui/react';
 import { ReactElement, useState } from 'react';
 
 import { Canvas } from '@react-three/fiber';
 
 import Box from './HomePage/Cubes';
 import { useNavigate } from 'react-router-dom';
+
+const ThreeDots = () : ReactElement => {
+    return (
+        <Canvas>
+            <ambientLight />
+            <pointLight position={[10, 10, 10]} />
+            <Box position={[-2.0, 0, 0]} />
+            <Box position={[0.0, 0, 0]} />
+            <Box position={[2.0, 0, 0]} />
+        </Canvas>
+    );
+}
 
 const Title = () : ReactElement => {
     const [hovered, hover] = useState(false);
@@ -15,14 +27,19 @@ const Title = () : ReactElement => {
     };
 
     return (
-        <VStack spacing="1px">
+        <VStack spacing='1'>
             <Text
                 onMouseEnter={() => hover(true)}
                 onMouseLeave={() => hover(false)}
                 color={hovered ? 'darkred' : 'black'}
-                fontSize='6xl' as='b'>
-                    type tier</Text>
-            <Button onClick={(navigateToGame)} variant='link' fontSize='xl' color='darkred'>
+                fontSize='6xl'>
+                type tier
+            </Text>
+            <Button
+                onClick={(navigateToGame)}
+                variant='link'
+                fontSize='l'
+                color='darkred'>
                 test your typing speed
             </Button>
         </VStack>
@@ -30,20 +47,13 @@ const Title = () : ReactElement => {
 }
 
 const HomePage = () : ReactElement => {
+
     return (
         <Center mt="220px">
-            <HStack spacing='1px'>
-                <VStack spacing='1px'>
-                <Title />
-                    <Canvas>
-                        <ambientLight />
-                        <pointLight position={[10, 10, 10]} />
-                        <Box position={[-2.0, 0, 0]} />
-                        <Box position={[0.0, 0, 0]} />
-                        <Box position={[2.0, 0, 0]} />
-                    </Canvas>
-                </VStack>
-            </HStack>
+            <Title />
+            <VStack spacing='1'>
+                <ThreeDots />
+            </VStack>
         </Center>
     );
 }
